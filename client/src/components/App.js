5f6926479b1c2bd9e08315b356f4f2cc1d9b19c4
GoAlek <alek.golaszewski@gmail.com>
2021-11-14 18:36:08 +0100

    implemented streams reducers and StreamList.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {StreamDelete} from './streams/StreamDelete';
 import {StreamEdit} from './streams/StreamEdit';
-import {StreamList} from './streams/StreamList';
 import {StreamShow} from './streams/StreamShow';
 import {Header} from './Header';
 import StreamCreate from './streams/StreamCreate';
+import StreamList from './streams/StreamList';
 
 const App = () => {
   return (
